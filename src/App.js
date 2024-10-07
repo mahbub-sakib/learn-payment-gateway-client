bae11de439d7f44d03ef9a7cadc85ca3d41b6cb8
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-10-07 06:21:19 +0600

    add react router dom for success fail cancel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import React from 'react';
 import CheckoutPage from './CheckoutPage';
+import Success from './Success';
+import Failed from './Failed';
+import Cancelled from './Cancelled';
 
 function App() {
   return (
     <div className="App">
-      <CheckoutPage />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<CheckoutPage />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/fail" element={<Failed />} />
+          <Route path="/cancel" element={<Cancelled />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Cancelled.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cancelled = () => {
+    return (
+        <div>
+            Payment Cancelled
+        </div>
+    );
+};
+
+export default Cancelled;
